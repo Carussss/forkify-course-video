5e583487d8ff575e5d22215069ee0c141a9dbea2
carussss <965613875@qq.com>
2023-04-19 14:14:13 +0800

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '4a6fa605-5608-4c75-945d-5fc2a21f5e59';
+export const MODAL_CLOSE_SEC = 2.5;
